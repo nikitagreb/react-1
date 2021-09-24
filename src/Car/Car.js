49b4e4be969cf7e0873868a4c3662de3f12a29e9
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-09-24 08:44:27 +0300

    2.10

---
+++ src/Car/Car.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+// function Car() {
+//     return <div>This is car</div>;
+// }
+
+// const car = () => {
+//     return (
+//         <div>This is car</div>
+//     )
+// }
+//
+// const car = () => (
+//     <div>
+//         <span>This is car</span>
+//         <strong>very small</strong>
+//     </div>
+// )
+
+export default () => (
+    <div>
+        <span>This is car</span>
+        <strong>very small</strong>
+    </div>
+)
