732d257ed0be8300318e2c4b9cfb6cc7f3ed02d9
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-09-25 11:08:17 +0300

    3.23

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,13 +1,32 @@
 import React from 'react'
-import 'Car.css'
+import './Car.css'
 
-export default props => (
-    <div className='Car'>
-        <h3>Car name: {props.name}</h3>
-        <p>
-            <strong>Year: {props.year}</strong>
-        </p>
-        <input type="text" onChange={props.handlerChangeName} value={props.name}/>
-        <button onClick={props.handlerDeleteCar}>Delete car</button>
-    </div>
-)
+export default props => {
+
+    const inputClasses = ['input']
+    if (props.name !== '') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if (props.name.length > 4 ) {
+        inputClasses.push('bold')
+    }
+
+    return (
+        <div className="car">
+            <h3>Car name: {props.name}</h3>
+            <p>
+                <strong>Year: {props.year}</strong>
+            </p>
+            <input
+                type="text"
+                className={inputClasses.join(' ')}
+                onChange={props.handlerChangeName}
+                value={props.name}
+            />
+            <button onClick={props.handlerDeleteCar}>Delete car</button>
+        </div>
+    )
+}
