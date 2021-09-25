0bed31fb1c688df3a99df13ffe205b44ffb2e371
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-09-25 10:03:05 +0300

    3.16

--- src/App.js
+++ src/App.js
@@ -13,8 +13,10 @@ class App extends Component {
         pageTitle: "React components"
     }
 
-    changeTitleHandler = () => {
-        console.log('Click')
+    changeTitleHandler = (newTitle) => {
+        this.setState({
+            pageTitle: newTitle
+        })
     }
 
     render() {
@@ -28,11 +30,23 @@ class App extends Component {
             <div className="App" style={divStyle}>
                 <h1>{this.state.pageTitle}</h1>
 
-                <button onClick={this.changeTitleHandler}>Change title</button>
+                <button onClick={this.changeTitleHandler.bind(this, 'Changed!')}>Change title</button>
 
-                <Car name={cars[0].name} year={cars[0].year}/>
-                <Car name="Mazda" year={2019}/>
-                <Car name="Audi" year={2017}/>
+                <Car
+                    name={cars[0].name}
+                    year={cars[0].year}
+                    onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
+                />
+                <Car
+                    name={cars[1].name}
+                    year={cars[1].year}
+                    onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
+                />
+                <Car
+                    name={cars[2].name}
+                    year={cars[2].year}
+                    onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
+                />
             </div>
         )
     }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default props => (
     <div>
         <h3>Car name: {props.name}</h3>
-        <strong>Year: {props.year}</strong>
+        <p>
+            <strong>Year: {props.year}</strong>
+        </p>
+        <button onClick={props.onChangeTitle}>Click</button>
     </div>
 )
