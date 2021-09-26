f888c85c193a24f41b3ccd1a8e6789b169090e59
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-09-26 22:32:10 +0300

    3.38

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,35 +1,9 @@
 import React from 'react'
-import Radium from 'radium'
 import {Component} from 'react'
 import classes from './Car.module.scss'
+import withClass from '../hoc/withClass'
 
 class Car extends Component {
-
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        console.log('Car shouldComponentUpdate', nextProps, nextState, nextContext)
-        return nextProps.name.trim() !== this.props.name.trim()
-    }
-
-    // componentWillUpdate(nextProps, nextState, nextContext) {
-    //     console.log('Car componentWillUpdate', nextProps, nextState, nextContext)
-    // }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('Car componentDidUpdate', prevProps, prevState, snapshot)
-    }
-
-    componentWillUnmount() {
-        console.log('Car componentWillUnmount')
-    }
-
-    static getDerivedStateFromProps(nextProps, prevState) {
-        console.log('Car getDerivedStateFromProps', nextProps, prevState)
-        return prevState
-    }
-
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log('Car getSnapshotBeforeUpdate', prevProps, prevState)
-    }
 
     render() {
 
@@ -39,23 +13,12 @@ class Car extends Component {
         } else {
             inputClasses.push(classes['red'])
         }
-
         if (this.props.name.length > 4 ) {
             inputClasses.push(classes['bold'])
         }
 
-        const style = {
-            border: '1px solid #ccc',
-            boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-            ':hover': {
-                border: '1px solid #aaa',
-                boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
-                cursor: 'pointer'
-            }
-        }
-
         return (
-            <div className={classes['car']} style={style}>
+            <>
                 <h3>Car name: {this.props.name}</h3>
                 <p>Year: <strong>{this.props.year}</strong></p>
                 <input
@@ -65,9 +28,9 @@ class Car extends Component {
                     value={this.props.name}
                 />
                 <button onClick={this.props.handlerDeleteCar}>Delete car</button>
-            </div>
+            </>
         )
     }
 }
 
-export default Radium(Car)
+export default withClass(Car, classes['car'])
